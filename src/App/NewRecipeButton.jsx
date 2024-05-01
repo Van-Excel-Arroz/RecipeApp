4984f8b221da342d54fa89f6aa9eaa3adad5c003
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-05-02 07:53:55 +0800

    change some styling

--- src/App/NewRecipeButton.jsx
+++ src/App/NewRecipeButton.jsx
@@ -4,42 +4,38 @@ import { useState } from 'react';
 import { Button, Tooltip } from '@mui/material';
 
 export default function NewRecipeButton({ addRecipe, isRecipeAdded, setIsRecipeAdded }) {
-  const [open, setOpen] = useState(false);
+	const [open, setOpen] = useState(false);
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+	const handleClickOpen = () => {
+		setOpen(true);
+	};
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+	const handleClose = () => {
+		setOpen(false);
+	};
 
-  return (
-    <>
-      <Tooltip title="Create Recipe" placement="top">
-        <Button
-          variant="contained"
-          onClick={handleClickOpen}
-          sx={{
-            height: '4rem',
-            minWidth: '4rem',
-            padding: '0',
-            borderRadius: '50%',
-            fontSize: '3rem',
-            position: 'fixed',
-            bottom: '40px',
-            right: '40px',
-          }}
-        >
-          +
-        </Button>
-      </Tooltip>
-      <RecipeFormDialog onAddRecipe={addRecipe} onHandleClose={handleClose} open={open} />
-      <RecipeAlert
-        state={isRecipeAdded}
-        setState={setIsRecipeAdded}
-        message={'Successfully Added Recipe!'}
-      />
-    </>
-  );
+	return (
+		<>
+			<Tooltip title="Create Recipe" placement="top">
+				<Button
+					variant="contained"
+					onClick={handleClickOpen}
+					sx={{
+						height: '4rem',
+						minWidth: '4rem',
+						padding: '0',
+						borderRadius: '50%',
+						fontSize: '3rem',
+						position: 'fixed',
+						bottom: '40px',
+						right: '30px',
+					}}
+				>
+					+
+				</Button>
+			</Tooltip>
+			<RecipeFormDialog onAddRecipe={addRecipe} onHandleClose={handleClose} open={open} />
+			<RecipeAlert state={isRecipeAdded} setState={setIsRecipeAdded} message={'Successfully Added Recipe!'} />
+		</>
+	);
 }
