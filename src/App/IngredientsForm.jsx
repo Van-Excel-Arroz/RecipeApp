1190c71616da0ec0f208ddbfe9daf24fd7bc0643
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-05-07 14:36:23 +0800

    added a textfield

--- src/App/IngredientsForm.jsx
+++ src/App/IngredientsForm.jsx
@@ -1,3 +1,24 @@
-export default function IngredientsForm(params) {
-	return <h1>Ingredients</h1>;
+import { TextField } from '@mui/material';
+
+export default function IngredientsForm({ recipe, handleInputChange, errors, emptyTextError }) {
+	return (
+		<>
+			<h1>Ingredients</h1>
+			<h3>What are the Ingredients?</h3>
+			<TextField
+				id="filled-multiline-flexible"
+				label="Ingredients"
+				variant="outlined"
+				fullWidth
+				sx={{ marginBottom: '16px' }}
+				multiline
+				rows={8}
+				name="ingredients"
+				value={recipe.ingredients}
+				onChange={handleInputChange}
+				error={errors.ingredients}
+				helperText={errors.ingredients ? emptyTextError : ''}
+			/>
+		</>
+	);
 }
