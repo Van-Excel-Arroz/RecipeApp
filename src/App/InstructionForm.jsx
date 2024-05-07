da2042cee1c67955ac1e90555b213427e43aac00
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-05-07 14:21:06 +0800

    added textfield to InstructionForm component

--- src/App/InstructionForm.jsx
+++ src/App/InstructionForm.jsx
@@ -1,3 +1,20 @@
-export default function InstructionForm() {
-	return <h1>Instruction</h1>;
+import { TextField } from '@mui/material';
+
+export default function InstructionForm({ recipe, handleInputChange, errors, emptyTextError }) {
+	return (
+		<TextField
+			id="filled-multiline-flexible"
+			label="Ingredients"
+			variant="outlined"
+			fullWidth
+			sx={{ marginBottom: '16px' }}
+			multiline
+			rows={4}
+			name="ingredients"
+			value={recipe.ingredients}
+			onChange={handleInputChange}
+			error={errors.ingredients}
+			helperText={errors.ingredients ? emptyTextError : ''}
+		/>
+	);
 }
