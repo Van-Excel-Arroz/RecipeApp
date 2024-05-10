666959cff6125ca80a40aa1985beef4cc0323034
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-05-10 09:38:54 +0800

    added functions to add and reduce serving buttons

--- src/App/Forms/OptionalDetailsForm.jsx
+++ src/App/Forms/OptionalDetailsForm.jsx
@@ -4,14 +4,23 @@ import { useState } from 'react';
 export default function OptionalDetailsForm(params) {
 	const [servings, setServings] = useState(0);
 
+	const addServing = () => {
+		setServings(servings + 1);
+	};
+
+	const reduceServing = () => {
+		if (servings > 0) {
+			setServings(servings - 1);
+		}
+	};
 	return (
 		<>
 			<ButtonGroup variant="contained">
-				<Button>-</Button>
+				<Button onClick={reduceServing}>-</Button>
 				<Button variant="outlined" disabled>
-					<Typography sx={{ color: '#000' }}>0</Typography>
+					<Typography sx={{ color: '#000', p: '0 0.5rem' }}>{servings}</Typography>
 				</Button>
-				<Button>+</Button>
+				<Button onClick={addServing}>+</Button>
 			</ButtonGroup>
 		</>
 	);
