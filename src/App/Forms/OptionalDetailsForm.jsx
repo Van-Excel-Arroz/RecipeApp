672481de739fe46dfd727b5fc338f67f1ce3db2b
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-05-10 09:32:39 +0800

    added useState for Servings

--- src/App/Forms/OptionalDetailsForm.jsx
+++ src/App/Forms/OptionalDetailsForm.jsx
@@ -1,13 +1,18 @@
 import { Button, ButtonGroup, Typography } from '@mui/material';
+import { useState } from 'react';
 
 export default function OptionalDetailsForm(params) {
+	const [servings, setServings] = useState(0);
+
 	return (
-		<ButtonGroup variant="contained">
-			<Button>-</Button>
-			<Button variant="outlined" disabled>
-				<Typography sx={{ color: '#000' }}>0</Typography>
-			</Button>
-			<Button>+</Button>
-		</ButtonGroup>
+		<>
+			<ButtonGroup variant="contained">
+				<Button>-</Button>
+				<Button variant="outlined" disabled>
+					<Typography sx={{ color: '#000' }}>0</Typography>
+				</Button>
+				<Button>+</Button>
+			</ButtonGroup>
+		</>
 	);
 }
