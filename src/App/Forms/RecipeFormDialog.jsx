91158df25d7c1e01ca35aa4a7b754b305400f339
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-05-24 15:22:06 +0800

    fixed a typo

--- src/App/Forms/RecipeFormDialog.jsx
+++ src/App/Forms/RecipeFormDialog.jsx
@@ -60,7 +60,7 @@ export default function RecipeDialog({ onAddRecipe, onHandleClose, open }) {
 				newErrors[key] = true;
 				isFormValid = false;
 			} else {
-				newErrobnppoprs[key] = false;
+				newErrors[key] = false;
 			}
 		});
 
