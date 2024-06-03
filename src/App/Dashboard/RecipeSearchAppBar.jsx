c27966b88e7d50e49b979525df5f2a35c51a95c7
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-06-03 15:57:45 +0800

    change the InputBase to TextField for Search bar

--- src/App/Dashboard/RecipeSearchAppBar.jsx
+++ src/App/Dashboard/RecipeSearchAppBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Toolbar, IconButton, InputBase, Box, Divider } from '@mui/material';
+import { AppBar, Toolbar, IconButton, TextField, Box, Divider } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import FilterListIcon from '@mui/icons-material/FilterList';
 import { useState } from 'react';
@@ -15,19 +15,11 @@ export default function RecipeSearchAppBar({ filterRecipes, setSearchTerm }) {
 	};
 
 	return (
-		<Box sx={{ display: 'flex', justifyContent: 'flex-end', mb: '2rem' }}>
-			<AppBar position="static" sx={{ width: '30%' }}>
-				<Toolbar>
-					<InputBase placeholder="Search Recipe" fullWidth value={localSearchTerm} onChange={handleSearchChange} />
-					<IconButton color="inherit">
-						<SearchIcon />
-					</IconButton>
-					<Divider orientation="vertical" sx={{ height: '28px', mx: 1 }} />
-					<IconButton color="inherit">
-						<FilterListIcon />
-					</IconButton>
-				</Toolbar>
-			</AppBar>
+		<Box sx={{ display: 'flex', justifyContent: 'space-between', mb: '2rem' }}>
+			<TextField placeholder="Search Recipe" fullWidth value={localSearchTerm} onChange={handleSearchChange} />
+			<IconButton color="inherit">
+				<FilterListIcon />
+			</IconButton>
 		</Box>
 	);
 }
