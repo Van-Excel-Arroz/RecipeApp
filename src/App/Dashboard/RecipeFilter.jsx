adfd657204117f72f3682703b7dbee6493674edc
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2024-06-06 15:09:43 +0800

    added a tooltip to filter iconbutton

--- src/App/Dashboard/RecipeFilter.jsx
+++ src/App/Dashboard/RecipeFilter.jsx
@@ -1,12 +1,14 @@
 import FilterListIcon from '@mui/icons-material/FilterList';
-import { IconButton } from '@mui/material';
+import { IconButton, Tooltip } from '@mui/material';
 
 export default function RecipeFilter() {
 	return (
 		<>
-			<IconButton>
-				<FilterListIcon />
-			</IconButton>
+			<Tooltip title="Filter">
+				<IconButton>
+					<FilterListIcon />
+				</IconButton>
+			</Tooltip>
 		</>
 	);
 }
